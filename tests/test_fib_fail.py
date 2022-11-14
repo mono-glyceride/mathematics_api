63a893ec6102aba710d2641d7a89f4db88a22fc8
mono-glyceride <glyceride.mono@gmail.com>
2022-11-15 01:45:20 +0900

    add 0より小さい値が渡された際の異常系テスト

--- tests/test_fib_fail.py
+++ tests/test_fib_fail.py
@@ -1,2 +1,14 @@
 import pytest
 from app.app import app
+
+def test_fail_too_small_1():
+  result = app.test_client().get('/fib?n=-1')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
+
+def test_fail_too_small_2():
+  result = app.test_client().get('/fib?n=0')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
