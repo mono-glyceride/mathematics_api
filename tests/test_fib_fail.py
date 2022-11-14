aa5cc9a98fb8dd139fa89f524adbe37b0cd080b9
mono-glyceride <glyceride.mono@gmail.com>
2022-11-15 01:46:38 +0900

    add 数値以外の値が渡された際の異常系テスト

--- tests/test_fib_fail.py
+++ tests/test_fib_fail.py
@@ -12,3 +12,27 @@ def test_fail_too_small_2():
   body = result.get_json()
   assert 400 == body['status']
   assert 400 == result.status_code
+
+def test_fail_not_integer_1():
+  result = app.test_client().get('/fib?n=あ')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
+
+def test_fail_not_integer_2():
+  result = app.test_client().get('/fib?n=u')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
+
+def test_fail_not_integer_3():
+  result = app.test_client().get('/fib?n=0E')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
+
+def test_fail_not_integer_4():
+  result = app.test_client().get('/fib?n=null')
+  body = result.get_json()
+  assert 400 == body['status']
+  assert 400 == result.status_code
