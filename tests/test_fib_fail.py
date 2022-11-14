835b38b0d2618636ec17f0d7ed2937bdf1484a0b
mono-glyceride <glyceride.mono@gmail.com>
2022-11-15 03:06:30 +0900

    update 1より小さい値が渡された際の異常系テストでエラーメッセージをテスト

--- tests/test_fib_fail.py
+++ tests/test_fib_fail.py
@@ -4,12 +4,14 @@ from app.app import app
 def test_fail_too_small_1():
   result = app.test_client().get('/fib?n=-1')
   body = result.get_json()
+  assert 'min value is 1' == body['message']['n'][0]
   assert 400 == body['status']
   assert 400 == result.status_code
 
 def test_fail_too_small_2():
   result = app.test_client().get('/fib?n=0')
   body = result.get_json()
+  assert 'min value is 1' == body['message']['n'][0]
   assert 400 == body['status']
   assert 400 == result.status_code
 
