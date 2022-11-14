b79bddee09943ee51178e274048a28a9f8eeebe0
mono-glyceride <glyceride.mono@gmail.com>
2022-11-15 03:57:16 +0900

    add 405エラー時にエラーメッセージとステータスをjsonで返す処理

--- app/app.py
+++ app/app.py
@@ -56,6 +56,10 @@ def error_status_code_400(message):
 def error_404(error):
   return set_error(404,'Not found. The requested resource does not exist.')
 
+@app.errorhandler(405)
+def error_405(error):
+  return set_error(405,'The method is not allowed for the requested URL.')
+
 def set_error(status, message):
   return jsonify({
   'status': status,
